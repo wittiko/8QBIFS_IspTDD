35624ddecf927dfde6b1f7818412258b39ab92aa
Gregor Messner <gm@hec-tech.net>
2012-09-25 12:16:56 +0200

    tests for valid ips ipv4 and ipv6

--- src/test/java/TransferTest.java
+++ src/test/java/TransferTest.java
@@ -62,6 +62,43 @@ public class TransferTest
 		assertThat(t1.getSize(), is(250));
 	}
 	
+	@Test
+	public void whenWeTryToSetAnInvalidIPAddress()
+	{
+		Transfer t1 = new Transfer();
+		thrown.expect(IllegalArgumentException.class);
+		t1.setSip("adasdas");
+		thrown.expect(IllegalArgumentException.class);
+		t1.setTip("dasda1313");
+		thrown.expect(IllegalArgumentException.class);
+		t1.setTip("532.129.232.1");
+		thrown.expect(IllegalArgumentException.class);
+		t1.setTip("2001:858:5:2c00:1:2");
+		
+		
+	}
+	
+	@Test
+	public void whenWeTryToSetAValidIPv4Address()
+	{
+		Transfer t1 = new Transfer();
+		t1.setSip("213.129.232.1");
+		t1.setTip("86.59.30.98");
+		assertThat(t1.getSip(), is("213.129.232.1"));
+		assertThat(t1.getTip(), is("86.59.30.98"));
+		
+	}
+	
+	@Test
+	public void whenWeTryToSetAValidIPv6Address()
+	{
+		Transfer t1 = new Transfer();
+		t1.setSip("2001:858:745::1");
+		t1.setTip("2001:858:5:2c00::2");
+		assertThat(t1.getSip(), is("2001:858:745::1"));
+		assertThat(t1.getTip(), is("2001:858:5:2c00::2"));
+	}
+	
 	
 	
 	
