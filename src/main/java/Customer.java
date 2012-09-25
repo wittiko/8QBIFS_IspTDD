ffac680e76ad032d48af47f8153243a588073fda
Gregor Messner <gm@hec-tech.net>
2012-09-25 12:49:21 +0200

    sumquota impl

---
+++ src/main/java/Customer.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+
+
+public class Customer 
+{
+	private ArrayList<Transfer> transfers;
+	private ArrayList<Integer> quota;
+	
+	public Customer()
+	{
+		transfers = new ArrayList<Transfer>();
+		quota = new ArrayList<Integer>();
+	}
+	
+	
+	// getter
+	
+	/**
+	 * @return the transfers
+	 */
+	public ArrayList<Transfer> getTransfers() 
+	{
+		return transfers;
+	}
+
+
+
+	/**
+	 * @return the quota
+	 */
+	public ArrayList<Integer> getQuota() 
+	{
+		return quota;
+	}
+
+
+
+	// setter
+	
+	/**
+	 * @param transfers the transfers to set
+	 */
+	public void setTransfers(ArrayList<Transfer> transfers) 
+	{
+		if(transfers != null)
+		{
+			this.transfers = transfers;
+		}
+		else
+		{
+			throw new NullPointerException("NULL @ setTransfers");
+		}
+		
+	}
+
+
+
+	/**
+	 * @param quota the quota to set
+	 */
+	public void setQuota(ArrayList<Integer> quota) 
+	{
+		if(quota != null)
+		{
+			this.quota = quota;
+		}
+		else
+		{
+			throw new NullPointerException("NULL @ setQuota");
+		}
+	}
+	
+	
+	public Integer SumQuotas()
+	{
+		Integer res = new Integer(0);
+		int count = 0;
+		while (count < quota.size()) 
+		{
+			res += quota.get(count);
+			count++;
+				
+		}
+		return res;
+	}
+
+
+		
+	
+}
